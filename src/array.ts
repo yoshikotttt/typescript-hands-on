42354d68b95195ea3b11a8f63f334ac9d7647970
yoshikotttt <myqic.n@gmail.com>
2023-11-15 14:20:44 +0900

    primitive-plactice

---
+++ src/array.ts
@@ -0,0 +1,20 @@
+export {};
+
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1,2,3];
+let strings2: Array<string> = ['tokyo','osaka','kyoto'];
+
+
+let strings: string[] = ['typescript','js','coffee'];
+
+let array2: number[][] = [
+  [50, 100],
+  [100, 200],
+];
+
+let array3: (string | number | boolean)[] = [1, false, "japan"];
+
+let profile: [string,number] = ['ham',43];
+// profile = [43,"cat"]
---
+++ src/interfaces.ts
@@ -0,0 +1,17 @@
+export {};
+
+type ObjectType = {
+  name: string;
+  age: number;
+}; 
+
+
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+let object:ObjectType = {
+  name: "cat",
+  age: 5,
+};
